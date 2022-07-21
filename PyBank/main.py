766d8846903f7950d5ef808225bb8db6364ad231
AThompson7 <108211958+AThompson7@users.noreply.github.com>
2022-07-21 21:28:31 +0800

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,11 @@
+#importing dependencies
 import os
 import csv
 
+#setting file path
 bank_csv = os.path.join("Resources", "budget_data.csv")
 
+#defining variables
 month = 0
 months = []
 net_profit = 0
@@ -13,7 +16,7 @@ maxmonth = ' '
 maxdecrease = 0
 maxdecrease_month = ' '
 average_change = 0
-
+#looping through the rows and storing values and lists
 with open(bank_csv, encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
@@ -34,7 +37,7 @@ with open(bank_csv, encoding='utf-8') as csv_file:
         maxdecrease_month = profit_change.index(min(profit_change))+1
         average_change = sum(profit_change) / len(profit_change)
 
-         
+#printing results         
 print("Financial Analysis")
 print("--------------------------------")
 print(f'Total months: {month}')
@@ -43,6 +46,7 @@ print(f'Average Change: ${round(average_change, 2)}')
 print(f'Greatest Increase in Profits: {months[maxmonth]} (${maxincrease})')
 print(f'Greatest Decrease in Profits: {months[maxdecrease_month]} (${maxdecrease})')
 
+#writing results to .txt file in the correct path
 output_path = os.path.join("analysis", "Financial Analysis.txt")
 with open(output_path, "w") as file:
  file.write("Financial Analysis" "\n")
